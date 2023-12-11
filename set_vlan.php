7e6744a7587e0218f393bbbff7752cdd5fd69654
sbonkov <sbonkov@gmail.com>
2023-12-11 16:55:15 +0200

    Update set_vlan.php
    Fixed for PHP version 81

--- set_vlan.php
+++ set_vlan.php
@@ -9,10 +9,8 @@ $vlan = $_POST['vlan'];
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 $extra = "index.php?page=onu&olt=$ip&mac=$mac";
-
-
-snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.101.12.1.1.18.$iface.$port", i, "1");
-snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.101.12.1.1.3.$iface.$port", i, $vlan);
-snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.20.15.1.1.0", i, "1");
+snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.101.12.1.1.18.$iface.$port", 'i', "1");
+snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.101.12.1.1.3.$iface.$port", 'i', $vlan);
+snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.20.15.1.1.0", 'i', "1");
 header("Location: http://$host$uri/$extra");
 ?>
