500b0e777530d5884180228e6c26ca1d5b4876d8
sbonkov <sbonkov@gmail.com>
2024-01-04 10:40:11 +0200

    Update unset_vlan.php

--- unset_vlan.php
+++ unset_vlan.php
@@ -8,7 +8,7 @@ $port = $_GET["port"];
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 $extra = "index.php?page=onu&olt=$ip&mac=$mac";
-snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.101.12.1.1.18.$iface.port", i, "0");
-snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.20.15.1.1.0", i, "1");
+snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.101.12.1.1.18.$iface.port", 'i', "0");
+snmp2_set($ip, $rw, "1.3.6.1.4.1.3320.20.15.1.1.0", 'i', "1");
 header("Location: http://$host$uri/$extra");
 ?>
