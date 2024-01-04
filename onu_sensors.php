9f16a169a10e5ebc39e30f1bcd81f76b8453135b
sbonkov <sbonkov@gmail.com>
2024-01-04 11:14:32 +0200

    Update onu_sensors.php

--- onu_sensors.php
+++ onu_sensors.php
@@ -1,7 +1,8 @@
 <?php
 echo "<div style=\"display: table; text-align: left; white-space: nowrap; width: 100%;\">";
 echo "<h1>".$comments."</h1>";
-if ($edit == "true" OR ($code == NULL AND $comments == NULL)) {
+//if ($edit == "true" OR ($code == NULL AND $comments == NULL)) {
+if ($edit == "true" OR ($comments == NULL)) {
 echo "<br/>";
                                                                  if ($use_userside == "yes") {
                                                                  include 'onu_code.php';
@@ -22,24 +23,25 @@ $onu_ip = ip2long($_GET['olt']);
 $sql = "select * from olts where ip=$onu_ip";
 $retval = $conn->query($sql);
 $row = $retval->fetch_array(MYSQLI_BOTH);
-//OLT IP
 $olt = $_GET["olt"];
 $place = $row['place'];
 $vendor_id = GetVendorID($ip, $ro, $iface);
 $model_id = GetModelID($ip, $ro, $iface);
 $onu_alivetime = GetOnuAliveTime($ip, $ro, $iface);
 $conn->close();
-echo "<br/><br/><br/>";
+//echo "<br/>";
 echo "<div style=\"float: left; display: table-cell; vertical-align: middle; top: 50%;\">";
 echo "<b>";
 $ipaddr=long2ip($row['ip']);
-echo "OLT: <a href=\"http://$olt\" target=\"_blank\">$place</a>";
+echo "OLT: <a href=\"http://$olt\" target=\"_blank\"><b>$place</b></a>";
 echo "<br>";
 echo "<br>";
+#echo "<a href=\"?page=onu&olt=$ip&mac=$mac\">$nameint</a>";
 echo $nameint;
 echo "</b><br/>";
 echo "<b>";
-echo "MAK: $mac";
+#echo "<a href=\"".$search_string.$mac."\" target=\"_blank\">";
+echo "МАК: $mac";
 echo "<br>";
 echo "Производител: $vendor_id";
 echo "<br>";
@@ -65,11 +67,12 @@ echo "</div>";
 echo "<div style=\"float: left; position:relative; left: 100px;\">";
 echo "ONU OFFLINE";
 } else {
-echo "<span class=\"rx\">$rx";
+echo "<span class=\"rx\">Нива: $rx";
 echo " dB</span><br/>";
 echo "<b>";
 echo "Дистанция: $dist";
 echo "</b><font size=2>m</font>";
+//echo "<br /><b><font size=2>$onu_alivetime</font></b>";
 echo "<br /><b><font size=2>Онлайн: ";
 echo secondsToTime($onu_alivetime);
 echo "</font></b>";
@@ -82,7 +85,7 @@ echo "<br/>";
 echo "<a href=\"?page=reboot_onu&olt=$ip&iface=$iface\"><img width=\"32\" src=\"images/reboot.png\"> Рестартиране на ONU</a>";
 echo "</font>";
 echo "</div>";
-echo "<div style=\"float: left; position: relative; padding-left: 100px; white-space: nowrap; line-height: 50px; display: inline; vertical-align: middle; top: 50%;\">";
+echo "<div style=\"float: left; position: relative; padding-left: 100px; white-space: nowrap; line-height: 50px; display: inline; vertical-align: middle; top: 20%;\">";
 include 'get_ports.php';
 }
 echo "</div>";
