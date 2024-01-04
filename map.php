878a92b28a9a258b915812d438900bc2e224c624
sbonkov <sbonkov@gmail.com>
2024-01-04 11:16:16 +0200

    Update map.php

--- map.php
+++ map.php
@@ -1,5 +1,5 @@
 <?php
-$maptype = $_GET["maptype"];
+$maptype = $_GET["maptype"] ?? null;
 if ($maptype == "heremap") {
 $layer = "heremapLayer";
 } else if ($maptype == "heresat") {
@@ -10,7 +10,6 @@ $layer = "osmLayer";
 $layer = "yandexLayer";
 }
 ?>
-
 <script type='text/javascript'>//<![CDATA[
 window.onload=function(){
 ym.ready(function() {
@@ -26,7 +25,6 @@ if ($layer == "yandexLayer") {
 }
 ?>
     }, {});
-
 <?php
 if ($layer == "yandexLayer") {
 } else {
@@ -36,36 +34,23 @@ if ($layer == "yandexLayer") {
         // указываем проекцию слоя OSM
         tileTransparent: true
     });
-
-
  var heremapLayer = new ym.Layer('http://1.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day/%z/%x/%y/256/png8?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg', {
         projection: ym.projection.sphericalMercator,
         // указываем проекцию слоя OSM
         tileTransparent: true
     });
 
-
-
     var osmLayer = new ym.Layer('http://otile%d.mqcdn.com/tiles/1.0.0/osm/%z/%x/%y.png', {
         projection: ym.projection.sphericalMercator,
         // указываем проекцию слоя OSM
         tileTransparent: true
     });
-
     map.layers.add(<?php echo $layer; ?>);
 <?php
 }
-
 include 'addmark.php';
 ?>
-
-
-
-
-
 map.behaviors.enable('scrollZoom');
-
-
     map.controls
         // Кнопка изменения масштаба.
         .add('zoomControl', { left: 5, top: 5 })
@@ -73,18 +58,7 @@ map.behaviors.enable('scrollZoom');
         .add('typeSelector')
         // Стандартный набор кнопок
         .add('mapTools', { left: 35, top: 5 });
-
-
-
-
-
-
 })
 }//]]>
-
 </script>
-
 <script src="http://api-maps.yandex.ru/2.0.7/?ns=ym&load=package.full&lang=en-US"></script>
-
-
-
