21405ac168080b615b5841325a03a5b0f436596a
sbonkov <sbonkov@gmail.com>
2024-01-04 10:52:23 +0200

    Update get_rw.php

--- get_rw.php
+++ get_rw.php
@@ -1,8 +1,6 @@
 <?php
-
 $conn = new mysqli($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
 $conn->set_charset("utf8");
-//mysql_select_db($mysql_db);
 $sql_ip = sprintf('%u', ip2long($ip));
 $sql = "select * from olts where ip='$sql_ip'";
 $retval = $conn->query( $sql );
@@ -10,8 +8,6 @@ if(! $retval )
 {
   die('Could not enter data: ' . mysqli_connect_error());
 }
-
-
   while ($row=$retval->fetch_array(MYSQLI_BOTH)) {
 $rw = $row['rw'];
 }
