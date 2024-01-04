230c59cdb8b088523e17aa15c88e15470b1e1947
sbonkov <sbonkov@gmail.com>
2024-01-04 11:08:43 +0200

    Update olt_list.php

--- olt_list.php
+++ olt_list.php
@@ -8,12 +8,19 @@
 <!--   <input type="submit" value="Търсене" style="border: none; font-size:22px;" />   -->
 <input type="submit" value="Търсене"  />
 </form>
+<!--
+   <form action="search.php" method="GET">
+       ТЕСТ: <input type="text" name="query" style="width:50%; height:30px; margin:5px; padding-left: 20px;" />
+        <input type="submit" value="Търсене" />
+    </form>
+-->
 <?php
 $conn = new mysqli($mysql_host, $mysql_user, $mysql_pass, $mysql_db );
 if($conn->connect_errno) {
     echo "Не удалось подключиться к MYSQL";
 }
 $conn->set_charset("utf8");
+$mac = $_GET["mac"] ?? null;
 $sql = "select * from onus where mac=$mac";
 $conn->close();
 ?>
@@ -24,20 +31,16 @@ $conn->close();
 include 'vars.php';
 $extra = 'index.php';
 $conn = new mysqli($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
-	// $conn = mysql_connect($mysql_host, $mysql_user, $mysql_pass);
 if($conn->connect_errno) {
 	echo "Не удалось подключиться к MYSQL";
 } 
 $conn->set_charset("utf8");
 $sql = "create database if not exists $mysql_db  default charset utf8";
 $retval = $conn->query( $sql );
-
 if(! $retval )
 {
   die('Could not enter data1: ' . mysqli_connect_error());
 }
-
-		//mysql_select_db($mysql_db);
 $sql = "CREATE TABLE IF NOT EXISTS olts (   `ip` INT UNSIGNED UNIQUE,   `place` varchar(256) DEFAULT NULL,   `ro` varchar(64) DEFAULT NULL,  `rw` varchar(64) DEFAULT NULL, `last_act` varchar(64) DEFAULT NULL, `lat` varchar(16) DEFAULT NULL, `lon` varchar(16) DEFAULT NULL, `olt_comments` varchar(256) DEFAULT NULL, `numsfp` varchar(16) DEFAULT NULL, `sfp1` varchar(256) DEFAULT NULL, `sfp2` varchar(256) DEFAULT NULL, `sfp3` varchar(256) DEFAULT NULL, `sfp4` varchar(256) DEFAULT NULL, `sfp5` varchar(256) DEFAULT NULL, `sfp6` varchar(256) DEFAULT NULL, `sfp7` varchar(256) DEFAULT NULL, `sfp8` varchar(256) DEFAULT NULL, `sfp9` varchar(256) DEFAULT NULL, `sfp10` varchar(256) DEFAULT NULL, `sfp11` varchar(256) DEFAULT NULL, `sfp12` varchar(256) DEFAULT NULL, `sfp13` varchar(256) DEFAULT NULL, `sfp14` varchar(256) DEFAULT NULL, `sfp15` varchar(256) DEFAULT NULL, `sfp16` varchar(256) DEFAULT NULL)";
 $retval = $conn->query( $sql );
 
