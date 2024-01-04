2cc4653547fbe86fb282139840048a341845494f
sbonkov <sbonkov@gmail.com>
2024-01-04 10:36:44 +0200

    Update ping.php

--- ping.php
+++ ping.php
@@ -1,13 +1,9 @@
 <?php
-
 include 'vars.php';
-
 $snmp_ping = snmp2_get($ip, $ro, "1.3.6.1.2.1.1.4.0");
 if (!$snmp_ping) {
 $ping = 0;
 } else {
-
-
 $ping = 1;
 $sql_ip = sprintf('%u', ip2long($ip));
 $extra = 'index.php';
@@ -20,6 +16,5 @@ if(! $retval_ping )
 {
   die('Could not enter data: ' . mysqli_connect_error());
 }
-
 }
 ?>
