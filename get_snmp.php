298de3c146e79b9a85a3ebb0194414bed485ae53
sbonkov <sbonkov@gmail.com>
2024-01-04 10:35:49 +0200

    Update get_snmp.php

--- get_snmp.php
+++ get_snmp.php
@@ -2,47 +2,35 @@
 include 'vars.php';
 $extra = 'index.php';
 if ($table == NULL) {
-$table = $_GET["olt"];
+$table = $_GET["olt"] ?? null;
 } else {
 }
-
 $ip = str_replace ("_", ".", $table);
-
 include 'get_ro.php';
 include 'get_rw.php';
 include_once 'function_lib.php';
-
 $sql_ip = ip2longfixed($ip);
-
 $conn = new mysqli($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
 $conn->set_charset("utf8");
-//mysql_select_db($mysql_db);
 UpdateOltLastAct($conn, $sql_ip, $date);
-
 $Array_descr = snmprealwalk($ip, $ro, ".1.3.6.1.4.1.3320.101.10.1.1.26");
-
- if(count($Array_descr)>0)
+ if(count((array)$Array_descr)>0)
  {
- foreach($Array_descr as $key => $type)
- {
-
-$key = end(explode('10.1.1.26.', $key));
-
-$type = NameById($ip, $ro, $key);
-
- $olt = strtok($type, ":");
-
- if(preg_match("#:#", $type))
- {
-$rx = RxById($ip, $ro, $key);
-$nameint = NameIntAddZero(NameById($ip, $ro, $key));
-$mac = MacById($ip, $ro, $key);
-UpdateOnu($conn, $sql_ip, $date, $nameint, $mac, $rx);
-
-}
-}
-
-}
-
+ foreach((array)$Array_descr as $key => $type)
+   {
+	//$key = end(explode('10.1.1.26.', $key));
+   $tmp = explode('10.1.1.26.', $key);
+   $key = end($tmp);
+   $type = NameById($ip, $ro, $key);
+   $olt = strtok($type, ":");
+		if(preg_match("#:#", $type))
+		{
+		$rx = RxById($ip, $ro, $key);
+		$nameint = NameIntAddZero(NameById($ip, $ro, $key));
+		$mac = MacById($ip, $ro, $key);
+		UpdateOnu($conn, $sql_ip, $date, $nameint, $mac, $rx);
+		}
+   }
+ }
 header("Location: http://$host$uri/$extra?page=olt&olt=$table");
 ?>
